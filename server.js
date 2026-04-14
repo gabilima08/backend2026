9b79e757bd26eaed0e1f3aabc5f18dd1d282771d
gabilima08 <gabrielly.vilant.lima@escola.pr.gov.br>
2026-04-14 07:51:03 -0300

    Atualização do arquivo server.js 14 de Abril

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
+  if(req.url === '/'){
     fs.readFile('index.html', (err, data) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
+      res.writeHead(200, {'Content-Type':'text/html'});
+      res.end(data);
+    });
+  }else{
+    if(req.url === '/style.css'){
+      fs.readFile('style.css', (err, data) => {
+        res.writeHead(200, {'Content-Type':'text/css'});
+        res.end(data);
+      });
+    }
+  }
 });
 
 server.listen(3000, () => {
